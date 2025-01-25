bb7185a980b335611014a55ecebc31957ec6f6fb
Ben Bokser <bbokser@gmail.com>
2025-01-25 18:13:40 -0500

    Troubleshooting 3d smooth contact

--- src/cube_3d_con_smooth.py
+++ src/cube_3d_con_smooth.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import plotting
 from transforms import Lq, Rq, H, Aq
@@ -10,7 +11,7 @@ import os_utils
 dt = 0.001
 g = 9.81
 m = 10  # mass of the particle in kg
-I = np.eye(3)  # inertia matrix
+I = np.eye(3) * 0.01  # inertia matrix
 dt = 0.001  # timestep size
 mu = 0.1  # coefficient of friction
 # Gravitational force in world frame
@@ -44,14 +45,15 @@ def kin_corners(X: np.ndarray) -> np.ndarray:
 
 
 def get_grf(z: float, dz: float) -> float:
-    k = 0.01  # spring constant
+    k = 0.01
     b = 0.1  # damping constant
-    amp = 1500  # desired max force
+    amp = 2e3  # desired max force
     c = amp * 0.5 / k
-    distance_fn = -c * np.tanh(z * 100) + c
+    distance_fn = -c * np.tanh(z * 5) + c
     F_spring = k * distance_fn
     F_damper = -b * dz * distance_fn
     grf = F_spring + F_damper
+    grf = np.clip(grf, 0, amp)
     return grf
 
 
@@ -68,21 +70,7 @@ def dynamics_ct(X: np.ndarray, U: np.ndarray) -> np.ndarray:
     # add gravity
     F_w += F_g_w
 
-    A = Aq(Q)
-    r_c_w = kin_corners(X)
-    for i in range(8):
-        # get height of corner in world frame
-        z = r_c_w[i, 2]
-        # get world frame force on corner
-        F_c_w = np.array((0, 0, get_grf(z, 0)))
-        # add to world frame force
-        F_w += F_c_w
-        # transform force from world frame back to body frame
-        F_c_b = A @ F_c_w
-        # add torque due to body frame force
-        tau_b += np.cross(r_c_b[i, :], F_c_b)
-
-    dr = v_w  # rotate v from body to world frame
+    dr = v_w
     dq = 0.5 * Lq(Q) @ H @ ω_b
     dv = 1 / m * F_w
     dω = np.linalg.solve(I, tau_b - np.cross(ω_b, I @ ω_b))
@@ -97,13 +85,12 @@ def rk4_normalized(X_k: np.ndarray, U_k: np.ndarray) -> np.ndarray:
     f2 = dynamics(X_k + 0.5 * dt * f1, U_k)
     f3 = dynamics(X_k + 0.5 * dt * f2, U_k)
     f4 = dynamics(X_k + dt * f3, U_k)
-    xn = X_k + (dt / 6.0) * (f1 + 2 * f2 + 2 * f3 + f4)
-    xn[3:7] = xn[3:7] / np.linalg.norm(xn[3:7])  # normalize the quaternion term
-    return xn
+    X_n = X_k + (dt / 6.0) * (f1 + 2 * f2 + 2 * f3 + f4)
+    X_n[3:7] = X_n[3:7] / np.linalg.norm(X_n[3:7])  # normalize the quaternion term
+    return X_n
 
 
-N = 1200  # number of timesteps
-
+N = 3000  # number of timesteps
 n_x = 13  # length of state vector
 n_u = 6  # length of control vector
 X_0 = np.zeros(n_x)
@@ -111,7 +98,7 @@ X_0 = np.zeros(n_x)
 p_0 = np.array([0, 0, 3.0])
 Q_0 = np.array([1, 0, 0, 0])
 v_0 = np.array([2, 0, 0])
-ω_0 = np.array([1, 0, 0])
+ω_0 = np.array([1, 0.5, 0])
 
 X_0[:3] = p_0
 X_0[3:7] = Q_0
@@ -119,20 +106,49 @@ X_0[7:10] = v_0
 X_0[10:13] = ω_0
 
 X_hist = np.zeros((N, n_x))  # array of state vectors for each timestep
-Fx_hist = np.zeros(N)  # array of x GRF forces for each timestep
-Fz_hist = np.zeros(N)  # array of z GRF forces for each timestep
+F_hist = np.zeros((N, 3))  # array of GRF vectors for each timestep
+tau_hist = np.zeros((N, 3))  # array of reaction torque vectors for each timestep
+
 X_hist[0, :] = X_0
 U_hist = np.zeros((N - 1, n_u))  # array of control vectors for each timestep
 
 for k in range(N - 1):
+    Q = X_hist[k, 3:7]  # B to W
+    v_w = X_hist[k, 7:10]  # W frame
+    ω_b = X_hist[k, 10:13]  # B frame
+    # rotation matrix, body to world frame
+    A = Aq(Q)
+    # world frame locations of the 8 corners of the cube
+    r_c_w = kin_corners(X_hist[k, :])
+    # iterate through corners
+    for i in range(8):
+        # get height of corner in world frame
+        z_i = r_c_w[i, 2]
+        # get z aspect of linear velocity of corner in world frame
+        dr_w_i = A @ np.cross(ω_b, r_c_b[i, :])
+        dz_i = dr_w_i[2]
+        # get world frame force on corner
+        F_c_w = np.array((0, 0, get_grf(z_i, dz_i)))
+        # add to world frame force
+        F_hist[k, :] += F_c_w
+        # transform force from world frame back to body frame
+        F_c_b = A.T @ F_c_w
+        # add torque due to body frame force
+        tau_hist[k, :] += np.cross(r_c_b[i, :], F_c_b)
+    U_hist[k, :3] = F_hist[k, :]
+    U_hist[k, 3:] = tau_hist[k, :]
     X_hist[k + 1, :] = rk4_normalized(X_hist[k, :], U_hist[k, :])
+    # if np.isnan(X_hist[k, 2]):
+    #     break
 
 
 name = "cube_3d_con_smooth"
 hists = {
-    "x (m)": X_hist[:, 0],
-    "y (m)": X_hist[:, 1],
     "z (m)": X_hist[:, 2],
+    "Fz (N)": F_hist[:, 2],
+    "tau_x (Nm)": tau_hist[:, 0],
+    "tau_y (Nm)": tau_hist[:, 1],
+    "tau_z (Nm)": tau_hist[:, 2],
 }
 plotting.plot_hist(hists, name)
 
@@ -149,13 +165,22 @@ for k in tqdm(range(N)[::frames]):
         y1 = r_c[i, 1]
         z1 = r_c[i, 2]
         ax.scatter(x1, y1, z1)
-    ax.set_xlim(-3, 3)
-    ax.set_ylim(-3, 3)
-    ax.set_zlim(0, 6)
+    ax.set_xlim(-6, 6)
+    ax.set_ylim(-6, 6)
+    ax.set_zlim(0, 12)
     ax.set_xlabel("x (m)")
     ax.set_ylabel("y (m)")
     ax.set_zlabel("z (m)")
     plt.title(name)
+    ax.text(
+        x=0.01,
+        y=0,
+        z=0,
+        s="t = " + "{:.2f}".format(round(k * dt, 2)) + "s",
+        ha="left",
+        va="bottom",
+        transform=plt.gca().transAxes,
+    )
     fig.savefig(path_dir_imgs + "/" + str(j).zfill(4) + ".png")
     plt.close()
     j += 1
